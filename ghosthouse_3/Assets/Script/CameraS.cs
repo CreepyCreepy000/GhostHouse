0bcffcdbbdf9924b528b9380ec0c024b71c7df3b
bbakbbaks <38639675+bbakbbaks@users.noreply.github.com>
2018-07-14 00:08:16 +0900

    Instantiate G1&W1

---
+++ ghosthouse_3/Assets/Script/CameraS.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraS : MonoBehaviour {
+    public float m_fSpeed = 5.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraMove();
+    }
+
+    public void CameraMove()
+    {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+        float fZaxis = Input.GetAxis("Mouse ScrollWheel");
+
+        if (Mathf.Abs(fVertical) > 0 || Mathf.Abs(fHorizontal) > 0 || Mathf.Abs(fZaxis) > 0)
+        {
+            float fYTranslation = fVertical * m_fSpeed;
+            float fXTranslation = fHorizontal * m_fSpeed;
+            float fZTranslation = fZaxis * m_fSpeed;
+            fXTranslation *= Time.deltaTime;
+            fYTranslation *= Time.deltaTime;
+            //fZTranslation *= Time.deltaTime;
+            transform.Translate(fXTranslation, fYTranslation, fZTranslation);
+        }
+
+        Vector3 limitmap;
+        limitmap.x = Mathf.Clamp(transform.position.x, -6, (float)6);
+        limitmap.y = Mathf.Clamp(transform.position.y, (float)-6, (float)6);
+        limitmap.z = Mathf.Clamp(transform.position.z, -20, 0);
+        transform.position = limitmap;
+
+
+    }
+}
---
+++ ghosthouse_3/Assets/Script/GameManager.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject Ghost1; //유령 오브젝트
+    public GameObject Wall1; //벽 오브젝트
+    public int PdCondition = 0; //상태를 나타내는 변수
+
+    static GameManager m_cInstance;
+
+    static public GameManager GetInstance()
+    {
+        return m_cInstance;
+    }
+
+    // Use this for initialization
+    void Start () {
+        m_cInstance = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        PurchaseArea();
+        //PdWall();
+        //PdGhost();
+        PreviewModel();
+        ProduceModel();
+    }
+
+    public void PurchaseArea()
+    {
+        //GameObject target = null;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo = new RaycastHit();
+        if (Physics.Raycast(ray, out hitinfo, 100.0f))
+        {
+            if(hitinfo.collider.gameObject.tag=="area")
+            {
+                if (Input.GetMouseButtonDown(0))
+                    Destroy(hitinfo.collider.gameObject);
+            }
+        }
+    }
+
+    //public void PdWall()
+    //{
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hitinfo = new RaycastHit();
+    //    if (Physics.Raycast(ray, out hitinfo, 100.0f))
+    //    {
+    //        if (hitinfo.collider.gameObject.tag == "Map")
+    //        {
+    //            //Debug.Log(hitinfo.collider.gameObject.tag);
+    //            if (Input.GetKeyDown(KeyCode.M))
+    //            {
+    //                //GameObject G_wall = Instantiate(Wall1, hitinfo.point, Quaternion.identity);
+    //                PdCondition = 1;
+    //            }
+    //        }
+    //    }
+    //}
+
+    //public void PdGhost()
+    //{
+    //    //GameObject target = null;
+    //    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    //    RaycastHit hitinfo = new RaycastHit();
+    //    if (Physics.Raycast(ray, out hitinfo, 100.0f))
+    //    {
+    //        if (hitinfo.collider.gameObject.tag == "Map")
+    //        {
+    //            //Debug.Log(hitinfo.collider.gameObject.tag);
+    //            if (Input.GetKeyDown(KeyCode.G))
+    //            {
+    //                //GameObject G_ghost = Instantiate(Ghost1, hitinfo.point, Quaternion.identity);
+    //                PdCondition = 2;
+    //            }
+    //        }
+    //    }
+    //}
+
+    public void ProduceModel()
+    {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            //GameObject G_wall = Instantiate(Wall1, hitinfo.point, Quaternion.identity);
+            PdCondition = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            //GameObject G_ghost = Instantiate(Ghost1, hitinfo.point, Quaternion.identity);
+            PdCondition = 2;
+        }
+    }
+
+    public void PreviewModel()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo = new RaycastHit();
+        if (Physics.Raycast(ray, out hitinfo, 100.0f))
+        {
+            if (PdCondition != 0)
+            {
+                //if (PdCondition == 1)
+                //{
+                //    Model = Instantiate(Wall1, hitinfo.point, Quaternion.identity);
+                //    //Destroy(WallModel);
+                //}
+
+                //if (PdCondition == 2)
+                //{
+                //    Model = Instantiate(Ghost1, hitinfo.point, Quaternion.identity);
+                //    //Destroy(GhostModel);   
+                //}
+            }
+        }     
+    }
+    
+}
